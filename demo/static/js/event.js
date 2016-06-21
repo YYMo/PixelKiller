2fcb9a12f8959a54869fe77bddfcbe5f5c5e0bd1
YY <yanyue1991@gmail.com>
2016-06-20 21:07:48 -0400

    implement event in js

---
+++ demo/static/js/event.js
@@ -0,0 +1,21 @@
+/**
+ * Event is simple implementation of Observer pattern
+ */
+
+function Event(sender) {
+  this._sender = sender;
+  this._listeners = [];
+
+}
+
+Event.prototype = {
+  attach: function(listener) {
+    this._listeners.push(listener);
+  },
+  notify: function(args){
+    var i;
+    for(i = 0; i < this._listeners.length; i += 1){
+      this._listeners[i](this._sender, args);
+    }  
+  }
+}
